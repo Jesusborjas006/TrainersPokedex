859a23cdacbd5a08f303208c59841d4fbb8f5b46
Jesusborjas006 <jesusbor006@gmail.com>
2024-08-22 18:52:13 -0500

    Add useQueries functionality to parallel requests for different pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import Navbar from "./ui/Navbar";
 import { usePokemonNames } from "./services/queries";
+import { useQueries } from "@tanstack/react-query";
+import { getPokemon } from "./services/api";
 
 function App() {
   const { data, isPending, isError } = usePokemonNames();
 
+  const pokemonDetails = useQueries({
+    queries: (data ?? [])?.map((pokemon) => {
+      return {
+        queryKey: ["pokemon", pokemon],
+        queryFn: () => getPokemon(pokemon),
+      };
+    }),
+  });
+
   if (isPending) return <span>Loading pokemon data...</span>;
 
   if (isError) return <span>Error fetching data</span>;
+
+  console.log(pokemonDetails);
 
   return (
     <>
