8e3b0175217fe5457e7fac4e09dfcd78229ebb4d
Jesusborjas006 <jesusbor006@gmail.com>
2024-08-22 18:41:16 -0500

    Create usePokemonNames hook to return useQuery that fetches just the names of pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
 import Navbar from "./ui/Navbar";
-import { getPokemonNames } from "./services/api";
+import { usePokemonNames } from "./services/queries";
 
 function App() {
-  const { data, isPending, isError } = useQuery({
-    queryKey: ["pokemon"],
-    queryFn: getPokemonNames,
-  });
-
-  console.log(data);
-
-  // const pokemons = useQueries({
-  //   queries: (data?.results ?? []).map((pokemon) => {
-  //     return {
-  //       queryKey: ["pokemons", pokemon.name],
-  //       queryFn: () => getPokemon(pokemon.name),
-  //     };
-  //   }),
-  // });
+  const { data, isPending, isError } = usePokemonNames();
 
   if (isPending) return <span>Loading pokemon data...</span>;
 
   if (isError) return <span>Error fetching data</span>;
 
-  // console.log(pokemons);
   return (
     <>
       <header>
         <Navbar />
       </header>
       <main>
-        {/* <ul>
-          {data.results.map((pokemon) => (
-            <li key={pokemon.name}>{pokemon.name}</li>
+        <ul>
+          {data.map((pokemon) => (
+            <li key={pokemon}>{pokemon}</li>
           ))}
-        </ul> */}
+        </ul>
       </main>
     </>
   );
---
+++ src/services/queries.ts
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPokemonNames } from "./api";
+
+export const usePokemonNames = () => {
+  return useQuery({
+    queryKey: ["pokemon"],
+    queryFn: getPokemonNames,
+  });
+};
