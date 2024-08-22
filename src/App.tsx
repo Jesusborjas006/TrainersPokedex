ec542ca6dadb728a75df960a17a9283c255cbc27
Jesusborjas006 <jesusbor006@gmail.com>
2024-08-22 18:36:57 -0500

    Create interface for pokemon and implement error handling for api

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
 import { useQuery } from "@tanstack/react-query";
 import Navbar from "./ui/Navbar";
-import { getPokemonId } from "./services/api";
+import { getPokemonNames } from "./services/api";
 
 function App() {
   const { data, isPending, isError } = useQuery({
     queryKey: ["pokemon"],
-    queryFn: getPokemonId,
+    queryFn: getPokemonNames,
   });
+
+  console.log(data);
+
+  // const pokemons = useQueries({
+  //   queries: (data?.results ?? []).map((pokemon) => {
+  //     return {
+  //       queryKey: ["pokemons", pokemon.name],
+  //       queryFn: () => getPokemon(pokemon.name),
+  //     };
+  //   }),
+  // });
 
   if (isPending) return <span>Loading pokemon data...</span>;
 
   if (isError) return <span>Error fetching data</span>;
 
-  console.log(data);
+  // console.log(pokemons);
   return (
     <>
       <header>
         <Navbar />
       </header>
       <main>
-        {data.results.map((pokemon) => (
-          <li key={pokemon.name}>{pokemon.name}</li>
-        ))}
+        {/* <ul>
+          {data.results.map((pokemon) => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))}
+        </ul> */}
       </main>
     </>
   );
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,28 @@
 const BASE_URL = "https://pokeapi.co/api/v2";
 
-export const getPokemonId = async () => {
+interface Pokemon {
+  count: number;
+  next: string;
+  previous: string;
+  results: {
+    name: string;
+    url: string;
+  }[];
+}
+
+export const getPokemonNames = async () => {
   const response = await fetch(`${BASE_URL}/pokemon`);
 
-  return response.json();
+  if (!response.ok) {
+    throw new Error("Error fetching data");
+  }
+
+  const data: Pokemon = await response.json();
+  return data.results.map((pokemon) => pokemon.name);
 };
+
+// export const getPokemon = async (name: string) => {
+//   const response = await fetch(`${BASE_URL}/pokemon/${name}`);
+
+//   return response.json();
+// };
