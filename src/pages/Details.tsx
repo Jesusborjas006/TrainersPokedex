ef0c9f6d28046589b70834aad61de43c6f24fee5
Jesusborjas006 <jesusbor006@gmail.com>
2024-08-28 13:38:07 -0500

    Add query to fetch from species api and display to details page

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,5 +1,6 @@
-import { UseQueryResult } from "@tanstack/react-query";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import { useNavigate, useParams } from "react-router-dom";
+import { getPokemonSpecies } from "../services/api";
 
 interface PokemonProps {
   pokemonDetails: UseQueryResult<
@@ -18,12 +19,16 @@ interface PokemonProps {
 const Details = ({ pokemonDetails }: PokemonProps) => {
   const navigate = useNavigate();
   const pokemonEndpoint = useParams();
-
   const pokemonData = pokemonDetails.find(
     (pokemon) => pokemon.data?.name === pokemonEndpoint.pokemon
   );
+  const pokemonSpecies = useQuery({
+    queryKey: ["species", pokemonEndpoint.pokemon],
+    queryFn: () => getPokemonSpecies(pokemonEndpoint.pokemon),
+    enabled: Boolean(pokemonData?.data?.name),
+  });
 
-  console.log(pokemonData?.data);
+  console.log(pokemonSpecies.data);
 
   if (!pokemonData)
     return (
@@ -43,6 +48,11 @@ const Details = ({ pokemonDetails }: PokemonProps) => {
         </ul>
         <p>Height: {pokemonData.data.height}</p>
         <p>Weight: {pokemonData.data.weight}</p>
+        <p>Growth Rate: {pokemonSpecies.data?.growth_rate.name}</p>
+        <p>
+          Pokedex Entry:{" "}
+          {pokemonSpecies.data?.flavor_text_entries[0].flavor_text}
+        </p>
       </div>
     );
   }
