5248813be8885b49abe61993d4ef8b4d580946af
Jesusborjas006 <jesusbor006@gmail.com>
2024-09-05 17:27:01 -0500

    Star styling added when clicked on

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { formattedPokemonId, typeColors } from "../utils/utils";
 import { TypeColorTypes } from "../types/pokemon";
 import Card from "../layouts/Card";
+import { useState } from "react";
 
 interface PokemonProps {
   id: number;
@@ -12,49 +13,53 @@ interface PokemonProps {
 }
 
 const Pokemon = ({ id, name, images, types, addToFavorites }: PokemonProps) => {
+  const [activeStar, setActiveStar] = useState(false);
   const handleClick = (e: { preventDefault: () => void }) => {
     e.preventDefault();
+    setActiveStar(true);
     addToFavorites(id);
   };
 
   return (
     <Card>
       <Link to={`${name}`}>
-        <div className="bg-yellow-500">
-          <button
-            className="border absolute top-2 left-2 inline-block bg-red-500"
-            onClick={handleClick}
-          >
-            Favorite
-          </button>
-          <p className="absolute right-2 top-2 text-sm text-blue-900">
-            #{formattedPokemonId(String(id))}
-          </p>
-          <div className="w-[120px] h-auto mx-auto">
-            <img
-              className="mx-auto bg-gray-300 rounded-full p-2"
-              src={images[0]}
-              alt={name}
-            />
-          </div>
-          <h3 className="text-lg font-medium">{name}</h3>
-          <div className="flex justify-center gap-x-2 mt-2 text-white">
-            {types.map((type) => (
-              <p
-                style={{
-                  backgroundColor: typeColors[type as keyof TypeColorTypes],
-                  padding: "2px 0",
-                  borderRadius: "4px",
-                  display: "inline-block",
-                  fontSize: "14px",
-                  width: "70px",
-                }}
-                key={type}
-              >
-                {type}
-              </p>
-            ))}
-          </div>
+        <button
+          className="absolute top-2 left-2 text-4xl hover:text-yellow-500 "
+          onClick={handleClick}
+        >
+          {activeStar ? (
+            <span className="text-yellow-400">★</span>
+          ) : (
+            <span>☆</span>
+          )}
+        </button>
+        <p className="absolute right-2 top-2 text-sm text-blue-900">
+          #{formattedPokemonId(String(id))}
+        </p>
+        <div className="w-[120px] h-auto mx-auto">
+          <img
+            className="mx-auto bg-gray-300 rounded-full p-2"
+            src={images[0]}
+            alt={name}
+          />
+        </div>
+        <h3 className="text-lg font-medium">{name}</h3>
+        <div className="flex justify-center gap-x-2 mt-2 text-white">
+          {types.map((type) => (
+            <p
+              style={{
+                backgroundColor: typeColors[type as keyof TypeColorTypes],
+                padding: "2px 0",
+                borderRadius: "4px",
+                display: "inline-block",
+                fontSize: "14px",
+                width: "70px",
+              }}
+              key={type}
+            >
+              {type}
+            </p>
+          ))}
         </div>
       </Link>
     </Card>
--- src/layouts/Card.tsx
+++ src/layouts/Card.tsx
@@ -1,6 +1,6 @@
 const Card = ({ children }) => {
   return (
-    <div className="border-4 cursor-pointer rounded-xl capitalize bg-slate-100  hover:border-red-600 relative">
+    <div className="border-4 cursor-pointer rounded-xl capitalize bg-slate-100 py-6 hover:border-red-600 relative">
       {children}
     </div>
   );
