63010bc725f3a27088e3df27e92d1dcd53a8bd09
Jesusborjas006 <jesusbor006@gmail.com>
2024-09-12 13:52:31 -0500

    Add second pokemon in new page

--- src/pages/PokemonStatsComparison.tsx
+++ src/pages/PokemonStatsComparison.tsx
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router";
 import { getPokemonDetails } from "../services/api";
 import { useState } from "react";
-import StatsBar from "../components/StatsBar";
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 const PokemonStatsComparison = () => {
@@ -11,6 +10,11 @@ const PokemonStatsComparison = () => {
   const { data, status } = useQuery({
     queryKey: ["comparisons", pokemonComparison1],
     queryFn: () => getPokemonDetails(pokemonComparison1),
+  });
+
+  const pokemonSelected2 = useQuery({
+    queryKey: ["comparisons", pokemonComparison2],
+    queryFn: () => getPokemonDetails(pokemonComparison2),
   });
   const navigate = useNavigate();
   const formattedStats = data?.stats.map((stat) => {
@@ -29,7 +33,7 @@ const PokemonStatsComparison = () => {
       <button className="my-6 text-white" onClick={() => navigate(-1)}>
         &larr; Go Back
       </button>
-      <div className="flex justify-around ">
+      <div className="flex justify-around">
         <div className="border bg-white w-[40%] rounded-xl">
           <div className="p-6">
             <img
@@ -56,9 +60,33 @@ const PokemonStatsComparison = () => {
             </ResponsiveContainer>
           </div>
         </div>
-        <div className="border">
-          <h3>Charizard</h3>
-          <p>Stats</p>
+        <div className="border bg-white w-[40%] rounded-xl">
+          <div className="p-6">
+            <img
+              className="bg-gray-300 rounded-lg"
+              src={pokemonSelected2.data?.sprites[1]}
+              alt={pokemonSelected2.data?.name}
+            />
+          </div>
+          <h3 className="text-center capitalize text-lg">
+            {pokemonSelected2.data?.name}
+          </h3>
+          <div className="h-[400px]">
+            <ResponsiveContainer
+              width="100%"
+              height="100%"
+              className="mx-auto relative"
+            >
+              <BarChart
+                data={formattedStats}
+                className="pb-10 text-xs md:text-sm lg:text-base"
+              >
+                <Bar dataKey="base_stat" fill="#8884d8" />
+                <XAxis dataKey="stat" interval={0} />
+                <YAxis dataKey="base_stat" domain={[0, 200]} />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
         </div>
       </div>
     </section>
