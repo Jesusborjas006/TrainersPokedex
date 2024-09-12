4a60433b142b43fa82644ff49cc5d35fe8ed5357
Jesusborjas006 <jesusbor006@gmail.com>
2024-09-12 13:01:18 -0500

    Add back button navigation

--- src/pages/PokemonStatsComparison.tsx
+++ src/pages/PokemonStatsComparison.tsx
@@ -1,8 +1,15 @@
+import { useNavigate } from "react-router";
+
 const PokemonStatsComparison = () => {
+  const navigate = useNavigate();
+  g;
   return (
-    <>
+    <section className="max-w-[1200px] mx-auto">
+      <button className="my-6 text-white" onClick={() => navigate(-1)}>
+        &larr; Go Back
+      </button>
       <h1>Stats</h1>
-    </>
+    </section>
   );
 };
 
