9df96c7efb50a4d721923d286f26f352770c59af
Jesusborjas006 <jesusbor006@gmail.com>
2024-09-13 15:16:13 -0500

    Fix the number id when pokemon ids reach over 1000 by displaying one less zero in the start

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,7 +7,9 @@ export const formattedPokemonId = (pokemonId: string) => {
   } else if (pokemonId.length === 2) {
     formattedId = `00${pokemonId}`;
   } else if (pokemonId.length === 3) {
-    formattedId = `00${pokemonId}`;
+    formattedId = `0${pokemonId}`;
+  } else {
+    formattedId = pokemonId;
   }
   return formattedId;
 };
