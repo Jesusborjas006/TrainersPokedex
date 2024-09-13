4bf74372d79fb6605c1fb90c3d107b4163765e8f
Jesusborjas006 <jesusbor006@gmail.com>
2024-09-13 13:35:22 -0500

    Move inputs outside the pokemon cards

--- src/pages/PokemonStatsComparison.tsx
+++ src/pages/PokemonStatsComparison.tsx
@@ -6,20 +6,22 @@ import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 import { formattedPokemonId } from "../utils/utils";
 
 const PokemonStatsComparison = () => {
+  const navigate = useNavigate();
   const [pokemonComparison1, setPokemonComparison1] = useState("");
   const [pokemonComparison2, setPokemonComparison2] = useState("");
-  const { data, status, isLoading } = useQuery({
+  const firstPokemon = useQuery({
     queryKey: ["comparisons", pokemonComparison1],
     queryFn: () => getPokemonDetails(pokemonComparison1),
     enabled: pokemonComparison1 !== "",
   });
 
-  const pokemonSelected2 = useQuery({
+  const secondPokemon = useQuery({
     queryKey: ["comparisons", pokemonComparison2],
     queryFn: () => getPokemonDetails(pokemonComparison2),
     enabled: pokemonComparison2 !== "",
   });
-  const navigate = useNavigate();
+
+  if (firstPokemon.status === "error") return <span>Error</span>;
 
   const formatStats = (pokemonStats) => {
     const formattedStats = pokemonStats?.stats.map((stat) => {
@@ -31,100 +33,140 @@ const PokemonStatsComparison = () => {
     return formattedStats;
   };
 
-  if (status === "error") return <span>Error</span>;
-
   return (
-    <section className="max-w-[1250px] mx-auto px-4">
+    <section className="max-w-[1250px] mx-auto px-4 ">
       <button className="my-6 text-white" onClick={() => navigate(-1)}>
         &larr; Go Back
       </button>
-      <div className="flex flex-col space-y-10 gap-x-10 md:flex-row md:space-y-0 ">
-        {status === "success" ? (
-          <div className="bg-white rounded-xl flex-1">
-            <div className="flex justify-center pt-6">
-              <label>Name or ID:</label>
-              <input
-                className="border pl-1 ml-1"
-                type="text"
-                placeholder="Enter pokemon name..."
-                value={pokemonComparison1}
-                onChange={(e) => setPokemonComparison1(e.target.value)}
-              />
-            </div>
-            <div className="p-6">
-              <img
-                className="bg-gray-300 rounded-lg mx-auto w-[200px] p-6"
-                src={data.sprites[1]}
-                alt={data.name}
-              />
-            </div>
-            <h3 className="text-center capitalize text-xl font-medium pb-4">
-              {data.name}
-              <span className=" font-light">
-                {" "}
-                | #{formattedPokemonId(String(data.id))}
-              </span>
-            </h3>
-            <div className="h-[300px] md:h-[350px]">
-              <ResponsiveContainer
-                width="100%"
-                height="100%"
-                className="mx-auto relative"
-              >
-                <BarChart
-                  data={formatStats(data)}
-                  className="pb-10 text-xs md:text-sm lg:text-base"
-                  margin={{ bottom: 70 }}
-                >
-                  <Bar dataKey="base_stat" fill="#8884d8" />
-                  <XAxis
-                    dataKey="stat"
-                    interval={0}
-                    angle={-45}
-                    textAnchor="end"
-                  />
-                  <YAxis dataKey="base_stat" domain={[0, 200]} />
-                </BarChart>
-              </ResponsiveContainer>
-            </div>
-          </div>
-        ) : (
-          <div className="bg-white rounded-xl flex-1 h-[692px]">
-            <div className="flex justify-center pt-6">
-              <label>Name or ID:</label>
-              <input
-                className="border pl-1 ml-1"
-                type="text"
-                placeholder="Enter pokemon name..."
-                value={pokemonComparison1}
-                onChange={(e) => setPokemonComparison1(e.target.value)}
-              />
-            </div>
-            <div className="w-[200px] h-[200px] bg-gray-300 mx-auto mt-6 rounded-lg"></div>
-            <h3 className="text-center capitalize text-xl font-medium py-6">
-              Undefined
-              <span className=" font-light"> | #</span>
-            </h3>
-          </div>
-        )}
-
-        <div className="bg-white rounded-xl flex-1 h-[692px]">
-          <div className="flex justify-center pt-6">
-            <label>Name or ID:</label>
-            <input
-              className="border pl-1 ml-1"
-              type="text"
-              placeholder="Enter pokemon name..."
-              value={pokemonComparison1}
-              onChange={(e) => setPokemonComparison1(e.target.value)}
-            />
-          </div>
-          <div className="w-[200px] h-[200px] bg-gray-300 mx-auto mt-6 rounded-lg"></div>
-          <h3 className="text-center capitalize text-xl font-medium py-6">
-            Undefined
-            <span className=" font-light"> | #</span>
-          </h3>
+      <div className="flex justify-around py-6 gap-x-10">
+        <div>
+          <label className="text-white">Name or ID:</label>
+          <input
+            className="border pl-1 ml-1"
+            type="text"
+            placeholder="Enter pokemon name..."
+            value={pokemonComparison1}
+            onChange={(e) => setPokemonComparison1(e.target.value)}
+          />
         </div>
+        <div>
+          <label className="text-white">Name or ID:</label>
+          <input
+            className="border pl-1 ml-1"
+            type="text"
+            placeholder="Enter pokemon name..."
+            value={pokemonComparison2}
+            onChange={(e) => setPokemonComparison2(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col space-y-10 gap-x-10 md:flex-row md:space-y-0">
+        {firstPokemon.status === "success" ? (
+          <>
+            <div className="bg-white rounded-xl flex-1 ">
+              <div className="p-6">
+                <img
+                  className="bg-gray-300 rounded-lg mx-auto w-[40%] p-4"
+                  src={firstPokemon.data.sprites[1]}
+                  alt={firstPokemon.data.name}
+                />
+              </div>
+              <h3 className="text-center capitalize text-xl font-medium pb-4">
+                {firstPokemon.data.name}
+                <span className=" font-light">
+                  {" "}
+                  | #{formattedPokemonId(String(firstPokemon.data.id))}
+                </span>
+              </h3>
+              <div className="h-[300px] md:h-[350px]">
+                <ResponsiveContainer
+                  width="100%"
+                  height="100%"
+                  className="mx-auto relative"
+                >
+                  <BarChart
+                    data={formatStats(firstPokemon.data)}
+                    className="pb-10 text-xs md:text-sm lg:text-base"
+                    margin={{ bottom: 70 }}
+                  >
+                    <Bar dataKey="base_stat" fill="#8884d8" />
+                    <XAxis
+                      dataKey="stat"
+                      interval={0}
+                      angle={-45}
+                      textAnchor="end"
+                    />
+                    <YAxis dataKey="base_stat" domain={[0, 200]} />
+                  </BarChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="bg-white rounded-xl flex-1">
+              <div className="flex flex-col justify-center text-center h-full">
+                <p className="p-10">
+                  No data to display. <br />
+                  Please fill the form to see Pokemon's stats.
+                </p>
+              </div>
+            </div>
+          </>
+        )}
+        {secondPokemon.status === "success" ? (
+          <>
+            <div className="bg-white rounded-xl flex-1">
+              <div className="p-6">
+                <img
+                  className="bg-gray-300 rounded-lg mx-auto w-[40%] p-4"
+                  src={secondPokemon.data.sprites[1]}
+                  alt={secondPokemon.data.name}
+                />
+              </div>
+              <h3 className="text-center capitalize text-xl font-medium pb-4">
+                {secondPokemon.data.name}
+                <span className=" font-light">
+                  {" "}
+                  | #{formattedPokemonId(String(secondPokemon.data.id))}
+                </span>
+              </h3>
+              <div className="h-[300px] md:h-[350px]">
+                <ResponsiveContainer
+                  width="100%"
+                  height="100%"
+                  className="mx-auto relative"
+                >
+                  <BarChart
+                    data={formatStats(secondPokemon.data)}
+                    className="pb-10 text-xs md:text-sm lg:text-base"
+                    margin={{ bottom: 70 }}
+                  >
+                    <Bar dataKey="base_stat" fill="#8884d8" />
+                    <XAxis
+                      dataKey="stat"
+                      interval={0}
+                      angle={-45}
+                      textAnchor="end"
+                    />
+                    <YAxis dataKey="base_stat" domain={[0, 200]} />
+                  </BarChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="bg-white rounded-xl flex-1 h-full">
+              <div className="flex flex-col justify-center text-center">
+                <p className="p-10">
+                  No data to display. <br />
+                  Please fill the form to see Pokemon's stats.
+                </p>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
