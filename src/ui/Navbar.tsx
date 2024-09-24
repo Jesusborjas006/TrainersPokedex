4e6bd61dc30a256e419138807aaf11955b3682d3
Jesusborjas006 <jesusbor006@gmail.com>
2024-09-24 12:54:56 -0500

    Add responsive design to nav bar

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -3,15 +3,15 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className="sticky top-0 z-10">
-      <nav className="bg-red-500 text-white px-10 py-2">
-        <div className="max-w-[1200px] flex items-end justify-between mx-auto sticky top-0">
-          <h2 className="text-xl">Trainers Pokedex</h2>
-          <div className="text-xl space-x-10">
+      <nav className="bg-red-500 text-white px-4 py-2">
+        <div className="max-w-[1200px] flex items-center justify-between mx-auto sticky top-0">
+          <h2 className="sm:text-lg">TrainersPokedex</h2>
+          <div className="space-x-2 text-sm sm:text-base sm:space-x-4">
             <NavLink to="/pokedex/favorites" className="hover:underline">
               Favorites
             </NavLink>
             <NavLink to="/pokedex/compare-stats" className="hover:underline">
-              Compare Stats
+              Compare-Stats
             </NavLink>
           </div>
         </div>
