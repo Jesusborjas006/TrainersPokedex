e6f78883fb1def7a7e07871e35d4d3f64ee0c4cc
Jesusborjas006 <jesusbor006@gmail.com>
2024-09-24 13:11:50 -0500

    Add responsive styling to SelectGeneration heading

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className="sticky top-0 z-10">
-      <nav className="bg-red-500 text-white px-4 py-2">
+      <nav className="bg-red-500 text-white px-4 py-2 sm:px-6 md:px-10">
         <div className="max-w-[1200px] flex items-center justify-between mx-auto sticky top-0">
-          <h2 className="sm:text-lg">TrainersPokedex</h2>
-          <div className="space-x-2 text-sm sm:text-base sm:space-x-4">
+          <h2 className="sm:text-lg font-medium">TrainersPokedex</h2>
+          <div className="space-x-2 text-sm sm:text-base sm:space-x-4 md:space-x-6">
             <NavLink to="/pokedex/favorites" className="hover:underline">
               Favorites
             </NavLink>
