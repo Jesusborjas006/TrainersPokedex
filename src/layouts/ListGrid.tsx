6e561f4439a136df3f0f5208f41de430306a9057
Jesusborjas006 <jesusbor006@gmail.com>
2024-09-25 17:25:50 -0500

    Add interface for ListGrid children prop

--- src/layouts/ListGrid.tsx
+++ src/layouts/ListGrid.tsx
@@ -1,4 +1,10 @@
-const ListGrid = ({ children }) => {
+import { ReactElement } from "react";
+
+interface ListGridProps {
+  children: ReactElement | ReactElement[];
+}
+
+const ListGrid = ({ children }: ListGridProps) => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 px-2 sm:px-6 md:px-10 gap-4 py-10 text-center max-w-[1250px] mx-auto">
       {children}
